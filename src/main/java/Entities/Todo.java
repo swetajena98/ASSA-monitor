c1b1e4fe0c45e9d23b6fc180e0c70f98acef96d1
sweta jena <38930598+swetajena98@users.noreply.github.com>
2021-12-12 16:49:27 +0530

    added entities

--- src/main/java/Entities/Todo.java
+++ src/main/java/Entities/Todo.java
@@ -3,11 +3,44 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Entities;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 /**
  *
  * @author Sciffer Laptop 10
  */
+@Entity
 public class Todo {
-    
+
+ // This tells Hibernate to make a table out of this class
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer id;
+  private Integer status;
+  private String title;
+
+  public Integer getId() {
+    return id;
+  }
+ public Integer getTitle() {
+    return title;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(Integer status) {
+    this.status = status;
+  }
+ public void setTitle(String title) {
+    this.title = title;
+  }
 }
+
