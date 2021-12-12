886eb244908ce0470261a1d95b3aa13d4a399366
Abhishek Tiwari <47275336+the-gero@users.noreply.github.com>
2021-12-12 16:34:43 +0530

    COntroller setup

---
+++ src/main/java/com/assa/demo/Controllers/HomeController.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.assa.demo.Controllers;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
